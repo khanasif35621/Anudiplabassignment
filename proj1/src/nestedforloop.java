84a6541c42a46aabb06c84a622ad02eeaef6b7f6
asif <asif@DESKTOP-52D8SHB>
2022-10-13 10:57:37 +0530

    Nestedforloop

---
+++ proj1/src/nestedforloop.java
@@ -0,0 +1,19 @@
+
+public class nestedforloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int x = 4;
+
+		    // outer loop
+		    for (int i = 1; i<=x;++i) {
+
+		      // inner loop to print the numbers
+		      for (int j = 1; j <= i; ++j) {
+		        System.out.print(j + " ");
+		      }
+		      System.out.println("");
+		    }
+		  }
+	
+}
