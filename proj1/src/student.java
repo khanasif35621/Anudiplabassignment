12eddceae31b7126c03da1ae90b9518237361d5f
asif <asif@DESKTOP-52D8SHB>
2022-10-13 10:16:54 +0530

    student

---
+++ proj1/src/student.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your roll no");
+		int x = sc.nextInt();
+		
+		if(x%4==1)
+		{
+			
+			System.out.println("you are in Emral group");
+			
+		}
+		else 
+			if(x%4==2)
+		{
+			
+				System.out.println("you are in Ruby group");
+			
+		}
+			else
+				if(x%4==3)
+				{
+					
+						System.out.println("you are in Pearls group");
+					
+				}
+				else 	if(x%4==0)
+				{
+					
+					System.out.println("you are in saphire group");
+				
+			}
+			
+		}
+}
