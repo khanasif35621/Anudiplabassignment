6e464210600bc27f7e9c594fe496146b2325f7a3
Asif <Asif@DESKTOP-KSG3AKA>
2022-11-10 11:46:52 +0530

    stringcases

---
+++ github/src/stringcases.java
@@ -0,0 +1,13 @@
+
+public class stringcases {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String txt = "Hello World";
+		String txt1="HI FRIENDS";
+		System.out.println(txt.toUpperCase());
+		System.out.println(txt1.toLowerCase());
+
+	}
+
+}
